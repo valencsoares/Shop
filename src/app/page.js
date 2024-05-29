5b5b196775de2f0dad86b52b4cb3f9909b9067e5
valencsoares <pvcampossoares558@gmail.com>
2024-05-29 17:45:00 -0400

    Feito a página Home, requirindo ao servidor "fakestore", feito o mapeamento para aparecer as informações necessárias. Utilizou o try e catch para retornar erro caso não consiga puxar as informações

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,28 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 import Main from "./componentes/Main";
+import style from "./page.module.css";
 
-export default function Home() {
+export default async function Home() {
+  try{
+    const resposta = await fetch("https://fakestoreapi.com/products", {nex:{revalidate: 1}});
+    const products = await resposta.json();
+
   return (
-    <main className={styles.main}>
+    <main style={{ marginTop: "10%" }}>
+      <div className={style.container}>
+        {products.map((prod) =>
+          <div className={style.card}>
+            <p>Name: {prod.title} </p>
+            <p>Price: {prod.price}</p>
+            <p>Description: {prod.description}</p>
+            <p>Category: {prod.category}</p>
+            <p>Rating: {prod.rating.count}</p>
+            <Image width={200} height={200} src={prod.image}/>
+          </div>)}
+      </div>
     </main>
   );
+} catch{
+  return <p style={{ marginTop: "10%" }}>Deu Erro!</p>
+}
 }
