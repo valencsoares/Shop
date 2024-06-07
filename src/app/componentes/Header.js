19906fb4a18cfd98fc09e05d95e0fd954b9222e9
valencsoares <pvcampossoares558@gmail.com>
2024-06-07 17:15:48 -0400

    Adicionado o botão de adicionar produto e alterado a posição e tamanho da logo e do carrinho, e alterado a posição dos itens no css

--- src/app/componentes/Header.js
+++ src/app/componentes/Header.js
@@ -5,13 +5,16 @@ export default function Header(){
     return (
         <header className={styles.cabecalho}>
             <div>
-                <Image width={200} height={200} src={'https://media.istockphoto.com/id/1423019666/pt/vetorial/reading-e-book-pixel-perfect-gradient-linear-ui-icon.jpg?s=612x612&w=0&k=20&c=myq2n2LjW78sV61KfeuOKGpMddPZxNKb7i8CpoQXWlo='}/>
+                <Image width={55} height={55} style={{ marginLeft: '2rem' }} src={'https://media.istockphoto.com/id/1423019666/pt/vetorial/reading-e-book-pixel-perfect-gradient-linear-ui-icon.jpg?s=612x612&w=0&k=20&c=myq2n2LjW78sV61KfeuOKGpMddPZxNKb7i8CpoQXWlo='} />
             </div>
             <div>
-                <p>Venha para nossa loja, aqui você encontrará tudo que precisa!</p>
+                <p>Venha para a nossa loja, aqui você encontrará tudo que precisa!</p>
             </div>
             <div>
-                <Image width={50} height={50} src={'https://media.istockphoto.com/id/1206806317/pt/vetorial/shopping-cart-icon-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=cl3NPzmnu0pRmaRGndYIejbHGe-atORf0JDjzmhX8Z8='}/>
+                <button>Adicionar produto</button>
+            </div>
+            <div>
+                <Image width={35} height={35} style={{ marginRight: '2rem' }} src={'https://media.istockphoto.com/id/1206806317/pt/vetorial/shopping-cart-icon-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=cl3NPzmnu0pRmaRGndYIejbHGe-atORf0JDjzmhX8Z8='} />
             </div>
         </header>
     )
